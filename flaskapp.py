439be5f1795cb042e9e1a6848e44c2902ae2260c
George <george.angelakis@outlook.com>
2026-03-24 11:42:51 -0500

    Exercise One Try One 13

--- flaskapp.py
+++ flaskapp.py
@@ -29,7 +29,17 @@ def hello(name):
 #  Each exercise asks you to add a new @app.route here
 # ============================================================
 
+# ---- Exercise 1: add this to flaskapp.py ----
 
+@app.route('/analyze/<word>')
+def analyze(word):
+    # YOUR CODE HERE
+    for i in len(word):
+        string += i
+    return str(string)
+    # Count the characters in `word` and return as a string
+    # Example: /analyze/Drake  →  should display: 5
+    pass
 
 
 # ============================================================
